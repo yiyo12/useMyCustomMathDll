594b12eadd9e6e9d3dc382d0dadeeb9a0f1d68b6
Carlos Coronado <yiyoskyrim@live.com>
2021-10-07 08:23:02 -0700

    using my custom math dll

---
+++ UsingMyCustomDll/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyCustomArithmeticLibrary;
+namespace UsingMyCustomDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MyCustomArithmeticLibrary.calculate customDll = new MyCustomArithmeticLibrary.calculate();
+            calculate customDll = new calculate();
+            var resultSuma =  customDll.sum(3, 5);
+            var resultSub = customDll.sub(5, 5);
+            Console.WriteLine(resultSuma + "  resultado de substracto  " +  resultSub);
+        }
+    }
+}
